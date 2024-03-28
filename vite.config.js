78c24decde9f87881fe06b471f3cf57790e1f1c0
ukattah <91033515+ukattah@users.noreply.github.com>
2024-03-28 11:21:07 -0400

    workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Calculator-App/",
+  base: "/Calculator-App-/",
   plugins: [react()],
 })
